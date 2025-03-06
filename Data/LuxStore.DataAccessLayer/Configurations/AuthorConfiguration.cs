bac6715dd1ee6e508e5dfd864f3f26a9e43e9ae8
Lütfullah Olgun <ltfllh99@gmail.com>
2025-03-06 15:02:07 +0300

    #newadds

---
+++ Data/LuxStore.DataAccessLayer/Configurations/AuthorConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using LuxStore.EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LuxStore.DataAccessLayer.Configurations
+{
+    internal class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.Property(a => a.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(a => a.Img)
+                .HasMaxLength(50);
+        }
+    }
+}
---
+++ Data/LuxStore.DataAccessLayer/Configurations/BooksConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using LuxStore.EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LuxStore.DataAccessLayer.Configurations
+{
+    internal class BooksConfiguration : IEntityTypeConfiguration<Books>
+    {
+        public void Configure(EntityTypeBuilder<Books> builder)
+        {
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+            builder.Property(p => p.Image)
+                .HasMaxLength(100);
+            builder.Property(p => p.BookCode)
+                .HasMaxLength(50);
+        }
+    }
+}
